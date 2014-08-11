8aadfb8378205287f113783a3eb67eb9423369fa
Vadim <rs.vsuslov@gmail.com>
2014-08-11 17:03:29 +0600

    init commit

---
+++ src/main/java/ru/rs/testcase/minrec/servlets/LoginServlet.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.rs.testcase.minrec.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "login", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String minister = req.getParameter("minister");
+        String url = "/";
+        if (minister.equals("")) {
+            url = "/ministerPage";
+        } else {
+            url = "/secretaryPage";
+        }
+        req.getRequestDispatcher(url).forward(null, null);
+    }
+}
